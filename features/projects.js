0559fda4266f3d7983e6f803e0d7b418e4969469
ezschrage <ezschrage@gmail.com>
2020-10-21 16:55:37 -0400

    project markup start, DS store git ignore

--- features/projects.js
+++ features/projects.js
@@ -5,39 +5,29 @@ const { BotkitConversation } = require('botkit');
 
 module.exports = function (controller) {
 
-    const projectNames = [];
     const quick_replies_projects = [];
+    const projectMarkup = resume.projects.map(project => {
+        
+    })
 
     resume.projects.forEach(name => {
-        projectNames.push(name.title)
+        let projectName = name.title
+
+        controller.hears(new RegExp(projectName, "i"), ['message'], async (bot, message) => {
+            await bot.reply(message, `<ul class="project_history">${projectMarkup}</ul>`);
+        });
+
         quick_replies_projects.push({
-            title: `${name.title}`,
-            payload: `${name.title}`
+            title: `${projectName}`,
+            payload: `${projectName}`
         })
     });
 
-    resume.projects.forEach(name => {
-        // let place = new RegExp(name.institution)
-        let place = name.institution
-        let partial = name.institution.split(' ')[0]
-        controller.hears(new RegExp(place, "i"), ['message'], async (bot, message) => {
-            await bot.reply(message, `${firstName} attended ${name.institution} from 
-            ${name.startDate} to ${name.endDate}. They pursued a ${name.studyType} for ${name.area}.`);
-        });
-        controller.hears(new RegExp(partial, "i"), ['message'], async (bot, message) => {
-            await bot.reply(message, `${firstName} attended ${name.institution} from 
-            ${name.startDate} to ${name.endDate}. They pursued a ${name.studyType} for ${name.area}.`);
-        });
-    });
-
-    controller.hears(new RegExp(/education|college|school|bootcamp|boot/, "i"), ['message'], async (bot, message) => {
+    controller.hears(new RegExp(/project|portfolio|experience/, "i"), ['message'], async (bot, message) => {
         await bot.reply(message, {
-            text: `Here is ${firstName}'s list of educational institutions.`,
-            quick_replies: quick_replies_institutions
+            text: `Here is ${firstName}'s list of projects.`,
+            quick_replies: quick_replies_projects
         })
     });
-
-    const educationInfo = new BotkitConversation('educationInfo', controller);
-
 
 };
